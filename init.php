c459d6e8b5f4f4012bb28fdf83b58ada178979d7
Thao Le <thaolt@songphi.com>
2015-09-30 19:43:24 +0700

    autodetect wiki base url

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 
 define('OXWIKI_DIR_ROOT', dirname(__FILE__));
 define('DOKUWIKI_DIR_ROOT', OXWIKI_DIR_ROOT . '/dokuwiki');
+
 define('OXWIKI_DIR_USERFILES', OW::getPluginManager()->getPlugin('oxwiki')->getUserFilesDir());
 define('OXWIKI_DIR_PLUGINFILES', OW::getPluginManager()->getPlugin('oxwiki')->getPluginFilesDir());
 
@@ -13,3 +14,5 @@ OW::getRouter()->addRoute(new OW_Route('oxwiki_threelvl', 'wiki/:any/:any1/:any2
 OW::getRouter()->addRoute(new OW_Route('oxwiki_fourlvl', 'wiki/:any/:any1/:any2/:any3', 'OXWIKI_CTRL_Doku', 'index'));
 OW::getRouter()->addRoute(new OW_Route('oxwiki_fivelvl', 'wiki/:any/:any1/:any2/:any3/:any4', 'OXWIKI_CTRL_Doku', 'index'));
 OW::getRouter()->addRoute(new OW_Route('oxwiki_sixlvl', 'wiki/:any/:any1/:any2/:any3/:any4/:any5', 'OXWIKI_CTRL_Doku', 'index'));
+
+define('OXWIKI_URL_BASE', OW::getRouter()->urlForRoute('oxwiki') );
